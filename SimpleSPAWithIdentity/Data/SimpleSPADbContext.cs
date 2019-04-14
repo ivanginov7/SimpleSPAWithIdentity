a1065f218695d381d7fe5864715aa47160724e22
ivanginov7 <ivanginov7@gmail.com>
2019-04-14 10:10:43 +0300

    Add project files.

---
+++ SimpleSPAWithIdentity/Data/SimpleSPADbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleSPAWithIdentity.Data
+{
+    public class SimpleSPADbContext:IdentityDbContext
+    {
+        public SimpleSPADbContext(DbContextOptions<SimpleSPADbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Entry> Entries { get; set; }
+    }
+}
